38c9c9836014d22a78ff547cfb002e358c8199f5
Ward Poelmans <wpoely86@gmail.com>
2021-01-14 20:59:27 +0100

    fix formatting with black

--- migrate-wiki.py
+++ migrate-wiki.py
@@ -10,24 +10,24 @@ def main():
         print("Need 2 arguments: <issue_map.json> <file.md>")
 
     # load issue map
-    with open(sys.argv[1], 'r') as json_in:
+    with open(sys.argv[1], "r") as json_in:
         issue_map = json.load(json_in)
 
-    issue_re = re.compile('#(?P<id>[0-9]+)')
+    issue_re = re.compile("#(?P<id>[0-9]+)")
 
-    with open(sys.argv[2], 'r') as md_in:
+    with open(sys.argv[2], "r") as md_in:
         md_file_in = md_in.read()
 
     md_file_out = md_file_in
 
     for hit in issue_re.finditer(md_file_in):
-        issue = hit.group('id')
+        issue = hit.group("id")
         if issue not in issue_map:
             continue
         print(f"Found one {hit.group(0)} in {sys.argv[2]}, mapping to {issue_map[issue][0]}")
-        md_file_out = re.sub(hit.group(0), f'#{issue_map[issue][0]}', md_file_out)
+        md_file_out = re.sub(hit.group(0), f"#{issue_map[issue][0]}", md_file_out)
 
-    with open(sys.argv[2], 'w') as md_out:
+    with open(sys.argv[2], "w") as md_out:
         md_out.write(md_file_out)
 
 
