a01175a51749f0f4fe175bf49b7dde66f851b8e5
Ward Poelmans <wpoely86@gmail.com>
2021-01-14 15:03:19 +0100

    Add wiki script

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-Redmine -> Azure DevOps Migration
-=================================
+# Redmine -> Azure DevOps Migration
 
-This repo has a script `migrate.py` that can you to migrate your Redmine issue
-to Azure DevOps work items.
+## Issues
+
+This repo has a script `migrate.py` that can help you to migrate your Redmine
+issues to Azure DevOps work items.
 
 It is *not* a fire and forgot kind of script but acts as a starting point
 for your own migration. To use it you have to:
@@ -19,3 +20,16 @@ It does not migrate everything perfectly. Caveats:
 - Tracker information is not used in Azure. We add it in a table at the bottom of the work item.
 - Redmine markdown is not 100% standard compliant (or at least not with the python markdown module).
   You may lose a bit of the formatting
+
+The script will also dump a json file with the mapping between redmine issue ids and azure work item ids.
+
+## wiki
+
+The script `wiki.py` will 'download' all the wiki pages and put them in a directory
+structure. This can then be added to the git repo of a Azure DevOps wiki. To find
+the git repo for a wiki, read [this](https://docs.microsoft.com/en-us/azure/devops/project/wiki/wiki-create-repo?view=azure-devops&tabs=browser#how-can-i-go-to-the-git-repository).
+
+
+### change issue numbers
+
+Still to do: change the issue number from redmine to azure in the wiki pages
---
+++ wiki.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+# this script will download a redmine wiki and create a set of files
+# and directories that can be copied to the git repo of a Azure DevOps wiki
+
+import os
+import re
+
+from redminelib import Redmine
+
+# url to the redmine
+redmine_url = "https://some.redmine.site.somewhere"
+# api key for redmine
+redmine_key = "REPLACE_WITH_REDMINE_API_KEY"
+
+
+def main():
+    redmine = Redmine(redmine_url, key=redmine_key)
+
+    project = redmine.project.get('some_project')
+
+    all_pages = dict()
+
+    # first, build a dict with all pages and their full path
+    for page in project.wiki_pages:
+        print(f"{page.title}")
+        comments = getattr(page, "comment", None)
+        uploads = getattr(page, "uploads", [])
+        if comments or uploads:
+            print(f"{page.title} has comments and/or attachments which we ignore")
+
+        parent = getattr(page, "parent", None)
+        full_path = []
+        while parent:
+            full_path.append(parent.title)
+            parent_page = redmine.wiki_page.get(parent.title, project_id=project)
+            parent = getattr(parent_page, "parent", None)
+        full_path.reverse()
+
+        all_pages[page.title] = (page, full_path)
+
+    for current in all_pages:
+        path = all_pages[current][1]
+        page = all_pages[current][0]
+        if path:
+            os.makedirs(os.sep.join(path), exist_ok=True)
+
+        output_file = os.path.join(os.sep.join(path), f"{page.title}.md")
+        content = page.text
+
+        for link in re.finditer(r'\[\[(?P<link>[^]]+)\]\]', content):
+            link_new = link.group('link').replace(' ', '_').replace('/', '')
+            # we want uppercase on first letter
+            link_new = link_new[0].upper() + link_new[1:]
+            if link_new not in all_pages:
+                continue
+            new_path = os.path.join(os.sep.join(all_pages.get(link_new)[1]), f"{link_new}.md")
+            content = re.sub(rf"\[\[{link.group('link')}\]\]", f"[{link.group('link')}](/{new_path})", content)
+
+        content = re.sub('{{toc}}', '[[_TOC_]]', content)
+        with open(output_file, "w") as output:
+            output.write(content)
+
+
+if __name__ == "__main__":
+    main()
